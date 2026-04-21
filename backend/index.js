d0e6b664a8577005dcbb705f89faf58f8daaffc2
divyanshurai27 <146620517+divyanshurai27@users.noreply.github.com>
2026-04-22 01:11:23 +0530

    index fixes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
+
+import  bookRoute from './routes/book.routes.js';
+
 const app = express()
 
 dotenv.config();
@@ -15,6 +18,7 @@ try {
     process.exit(1);
 }
 
+app.use("/book",bookRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is  listening on port ${PORT}`)
