4aa7b033be1b95885d243b4b2bbba5f2e90ae17d
divyanshurai27 <146620517+divyanshurai27@users.noreply.github.com>
2026-05-01 21:01:09 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
-
+import cors from 'cors';
 import  bookRoute from './routes/book.routes.js';
 
 const app = express()
@@ -11,14 +11,20 @@ const PORT  = process.env.PORT || 4000;
 const uri = process.env.MongoDBurl;
 
 try {
-    await mongoose.connect(uri)
-    console.log("mongoose connected" )
+  await mongoose.connect(uri)
+  console.log("mongoose connected" )
 } catch (error) {
-    console.log("Error:",error);
-    process.exit(1);
+  console.log("Error:",error);
+  process.exit(1);
 }
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use("/book",bookRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Server is  listening on port ${PORT}`)
