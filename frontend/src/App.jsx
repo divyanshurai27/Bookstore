e75f446709c70856a4f225e868c673ffa67db87f
divyanshurai27 <146620517+divyanshurai27@users.noreply.github.com>
2026-05-02 17:48:33 +0530

    bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import Home from "./home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
-
 import Contacts from "./contacts/Contacts";
+import toast ,{ Toaster } from 'react-hot-toast';
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
+
+  const[authUser,setAuthUser]= useAuth()
+console.log(authUser); 
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/course" element={<Courses />} />
+        <Route path="/course" element={authUser? <Courses />:<Navigate to='/signup'/>} />
          <Route path="/signup" element={<Signup />} />
           <Route path="/contact" element={<Contacts />} />
       </Routes>
+      <Toaster/>
     </>
   );
 }
